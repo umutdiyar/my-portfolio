e7c9f8aa43fc1f2f84babc90faba75725c19325f
umutdiyar <umutdiyarbalci@gmail.com>
2025-07-17 01:23:27 +0300

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="#">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="#">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="bg-white p-8 rounded-lg shadow-lg">
+          <h1 className="text-4xl font-bold text-red-600 mb-4">Vite + React</h1>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
