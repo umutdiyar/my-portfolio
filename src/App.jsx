16fe7618f984e867ffd7fd934b7ec183f7c5b6e1
umutdiyar <umutdiyarbalci@gmail.com>
2025-09-24 00:56:15 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import { motion } from "framer-motion";
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="bg-white p-8 rounded-lg shadow-lg">
-          <h1 className="text-4xl font-bold text-red-600 mb-4">Vite + React</h1>
-        </div>
+      <div className="flex h-screen items-center justify-center bg-gray-100">
+        <motion.div
+          className="p-6 bg-white rounded-2xl shadow-lg"
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          <h1 className="text-2xl font-bold">Framer Motion Çalışıyor 🚀</h1>
+          <h2 className="text-xl font-bold text-red-800 my-2">
+            ShadcnUi çalışıyor:
+          </h2>
+          <Button>Click Me</Button>
+          <p className="text-yellow-400 font-bold mt-2">
+            TailwindCSS Çalışıyor!
+          </p>
+        </motion.div>
       </div>
     </>
   );
